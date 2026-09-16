d00b0c34044d30510f016fdb208f79cba2e2e8c9
jaswanthg1040 <jaswanth.g.1040@gmail.com>
2026-09-16 21:20:33 +0530

    ✅ BUSSEAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.py
+++ codechef/medium/BUSSEAT/solution.py
@@ -2,6 +2,7 @@
 t = int(input())
 for i in range(t):
     n,k = map(int,input().split())
-    if (2*n) >= k :
+    if (2*n) >= k and k >= 0 :
         print((k // 2)* 2 )
-    
+    else:
+        print(k)
