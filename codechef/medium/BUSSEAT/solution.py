da5b2d7ea7f59fcc499d4cde2a2157b135593d91
jaswanthg1040 <jaswanth.g.1040@gmail.com>
2026-09-16 21:45:59 +0530

    ✅ BUSSEAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.py
+++ codechef/medium/BUSSEAT/solution.py
@@ -2,7 +2,7 @@
 t = int(input())
 for i in range(t):
     n,k = map(int,input().split())
-    if (2*n) > k:
-        print(max(0,(k-(n//2))))
+    if (2*n) >= k and k,n >= 0 :
+        print((k // 2)* 2 )
     else:
         print(k)
